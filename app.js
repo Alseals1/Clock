6ae671d603fcfc4f047204d3ca50e76134976042
Alandis Seals <alandis.seals@yahoo.com>
2020-06-20 13:27:17 -0500

    fixed hand timming

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-// function setDate() {
-//     const now = new Date()
-//     const seconds = now.getSeconds()
-//     const secondsDegrees = ((seconds / 60) * 360) + 90;
-//     // console.log(`${seconds} seconds`)
-//     const secondHand = document.querySelector('.second-hand')
-//     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+function setDate() {
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    // console.log(`${seconds} seconds`)
+    const secondHand = document.querySelector('.second-hand')
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
 
-//     const min = now.getMinutes()
-//     const minDegrees = ((min / 60) * 360) + 90
-//     const minHand = document.querySelector('.min-hand')
-//     minHand.style.transform = `rotate(${minDegrees}deg)`
-//     // console.log(`${min} minutes`)
+    const min = now.getMinutes()
+    const minDegrees = ((min / 60) * 360) + 90
+    const minHand = document.querySelector('.min-hand')
+    minHand.style.transform = `rotate(${minDegrees}deg)`
+    // console.log(`${min} minutes`)
 
 
 
-//     const hour = now.getHours()
-//     const hourDegrees = ((hour / 60) * 360) + 90
-//     const hourHand = document.querySelector('.hand-hour')
-//     hourHand.style.transform = `rotate(${hourDegrees}deg)`
-//     // console.log(`${hour} hour`)
-//     console.log(now)
+    const hour = now.getHours()
+    const hourDegrees = ((hour / 12) * 360) + 90
+    const hourHand = document.querySelector('.hand-hour')
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+    // console.log(`${hour} hour`)
+    console.log(now)
 
-// }
-// // setDate()
-// setInterval(setDate, 1000)
+}
+// setDate()
+setInterval(setDate, 1000)
